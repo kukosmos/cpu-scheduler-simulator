31d8b6a7dad1a9ef92a3fb0c54246463f1e4de68
HenryLee <gbvrcx@gmail.com>
2018-05-06 00:46:35 +0900

    implement process

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+#include "process.h"
+#include "clock.h"
+#include "job_scheduler.h"
+#include "cpu_scheduler.h"
+#include "cpu.h"
+#include "io_device.h"
+
 int main (int argc, char **argv) {
     printf("KOSMOS PROJECT\nCPU Scheduler Simulator\n");
 
