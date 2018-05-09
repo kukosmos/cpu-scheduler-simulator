50b57481a06fae3bf8438dd04baa59b128c1d412
HenryLee <gbvrcx@gmail.com>
2018-05-09 09:52:37 +0900

    basic print test

--- src/main.c
+++ src/main.c
@@ -10,5 +10,25 @@
 int main (int argc, char ** argv) {
     printf("KOSMOS PROJECT\nCPU Scheduler Simulator\n");
 
+    Clock * clk = create_clock ();
+    Job_scheduler * js = create_job_scheduler (clk);
+    CPU_scheduler * cs = create_cpu_scheduler ("fcfs", clk);
+    CPU * cpu = create_cpu (clk);
+    IO_device * io = create_io_device (clk);
+    
+    js_register_cpu_scheduler (js, cs);
+    register_cpu (cs, cpu);
+    register_io_device (cpu, io);
+    io_register_cpu_scheduler (io, cs);
+    
+    create_processes (js, 10);
+    print_processes (js);
+
+    delete_io_device (io);
+    delete_cpu (cpu);
+    delete_cpu_scheduler (cs);
+    delete_job_scheduler (js);
+    delete_clock (clk);
+
     return 0;
 }
