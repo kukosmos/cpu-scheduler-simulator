a3e69eb15c566c70269122dc5611ec91b8b364c1
HenryLee <gbvrcx@gmail.com>
2018-05-11 15:51:04 +0900

    rm thread from io device

--- src/io_device.h
+++ src/io_device.h
@@ -1,8 +1,6 @@
 #ifndef __IO_DEVICE_H
 #define __IO_DEVICE_H
 
-#include <pthread.h>
-
 #include "clock.h"
 #include "process.h"
 #include "record.h"
@@ -15,7 +13,6 @@ typedef struct __cpu_scheduler CPU_scheduler;
 
 struct __io_queue_node {
     Process * process;
-    struct __io_queue_node * prev;
     struct __io_queue_node * next;
 };
 
@@ -25,12 +22,11 @@ struct __io_queue {
 };
 
 struct __io_device {
-    int state;
+    Process * process;
     Clock * clk;
     Record * record;
     CPU_scheduler * cpu_scheduler;
     struct __io_queue * queue;
-    pthread_t tid;
 };
 
 typedef struct __io_device IO_device;
@@ -43,14 +39,6 @@ IO_device * create_io_device (Clock * clock);
  */
 void delete_io_device (IO_device * io_device);
 
-/* start io device
- */
-void start_io_device (IO_device * io_device);
-
-/* stop io device
- */
-void stop_io_device (IO_device * io_device);
-
 /* request io
  * if there are some process running enqueue
  * queue works FIFO
@@ -60,5 +48,9 @@ void io_request (IO_device * io_device, Process * process);
 /* register cpu scheduler
  */
 void io_register_cpu_scheduler (IO_device * io_device, CPU_scheduler * cpu_scheduler);
+
+/* execute io device for one clock cycle
+ */
+void ioing (IO_device * io_device);
 
 #endif
