e1cd7298274bc4d44ffaf86ca365a4b206f3e12c
HenryLee <gbvrcx@gmail.com>
2018-05-14 01:45:19 +0900

    update job_scheduler.h job_scheduler.c

--- src/job_scheduler.h
+++ src/job_scheduler.h
@@ -6,7 +6,7 @@
 #include "cpu_scheduler.h"
 
 struct __js_queue_node {
-    Process * p;
+    process_t * p;
     struct __js_queue_node * next;
 };
 
@@ -18,47 +18,45 @@ struct __js_queue {
     struct __js_queue_node * head;
 };
 
-typedef struct __js_queue JS_queue;
-
 struct __job_scheduler {
-    JS_queue * queue;
-    Clock * clk;
-    CPU_scheduler * cs;
+    struct __js_queue * queue;
+    clock_t * clk;
+    cpu_scheduler_t * cs;
 };
 
-typedef struct __job_scheduler Job_scheduler;
+typedef struct __job_scheduler job_scheduler_t;
 
 /* create job scheduler
  */
-Job_scheduler * create_job_scheduler (Clock * clock);
+job_scheduler_t * create_job_scheduler (clock_t * clock);
 
 /* delete job scheduler
  */
-void delete_job_scheduler (Job_scheduler * job_scheduler);
+void delete_job_scheduler (job_scheduler_t * job_scheduler);
 
 /* register cpu scheduler to job scheduler
  */
-void js_register_cpu_scheduler (Job_scheduler * job_scheduler, CPU_scheduler * cpu_scheduler);
+void js_register_cpu_scheduler (job_scheduler_t * job_scheduler, cpu_scheduler_t * cpu_scheduler);
 
 /* create n processes
  */
-void create_processes (Job_scheduler * job_scheduler, int n);
+void create_processes (job_scheduler_t * job_scheduler, int n);
 
 /* do scheduling for one clock cycle
  * give cpu a process which its arrival time = clock time
  */
-void job_scheduling (Job_scheduler * job_scheduler);
+void job_scheduling (job_scheduler_t * job_scheduler);
 
 /* restart job scheduling with existing process queue
  */
-void reset_job_scheduling (Job_scheduler * job_scheduler);
+void reset_job_scheduling (job_scheduler_t * job_scheduler);
 
 /* print processes informations to schedule
  */
-void print_processes (Job_scheduler * job_scheduler);
+void print_processes (job_scheduler_t * job_scheduler);
 
 /* return TRUE if all processes' remain time is 0
  */
-int all_terminated (Job_scheduler * job_scheduler);
+int all_terminated (job_scheduler_t * job_scheduler);
 
 #endif
