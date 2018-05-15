1967c0166ebcfbda5008450281ae4097d9bae1a4
HenryLee <gbvrcx@gmail.com>
2018-05-15 21:06:42 +0900

    np sjf implement

--- src/algorithm/np_sjf.c
+++ src/algorithm/np_sjf.c
@@ -1,15 +1,25 @@
 #include <stdlib.h>
 
+#include "sjf.h"
 #include "np_sjf.h"
 
-void * create_np_sjf_queue () {
+#define QUEUE struct __sjf_queue
 
+void * create_np_sjf_queue () {
+    return (void *) create_sjf_queue ();
 }
 
 void np_sjf_scheduling (cpu_scheduler_t * this) {
-
+    if (is_running (this->cpu)) {
+        return;
+    }
+    if (sjf_is_empty ((QUEUE *) this->queue)) {
+        return;
+    }
+    process_t * orig;
+    execute (this->cpu, sjf_dequeue ((QUEUE *) this->queue), &orig);
 }
 
 void np_sjf_enqueue (void * queue, process_t * process) {
-
+    sjf_enqueue ((struct __sjf_queue *) queue, process);
 }
