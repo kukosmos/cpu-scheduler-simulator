7c101ee0628778de4470a42b163297b622767b70
HenryLee <gbvrcx@gmail.com>
2018-05-16 22:06:04 +0900

    debug turn around time

--- src/evaluate.c
+++ src/evaluate.c
@@ -6,7 +6,7 @@ double evaluate_turn_around_time (int n, process_t ** pl, int * rl) {
     int sum = 0;
     int tt = 0;
     for (int i = 1; i <= n; i++) {
-        tt = pl[i]->termination_time - pl[i]->arrival_time;
+        tt = pl[i]->termination_time - pl[i]->arrival_time + 1;
         sum = sum + tt;
         rl[pl[i]->pid] = tt;
     }
@@ -17,7 +17,7 @@ double evaluate_waiting_time (int n, process_t ** pl, int * rl) {
     int sum = 0;
     int wt = 0;
     for (int i = 1; i <= n; i++) {
-        wt = pl[i]->termination_time - pl[i]->arrival_time - pl[i]->cpu_burst_time - pl[i]->io_burst_time;
+        wt = pl[i]->termination_time - pl[i]->arrival_time - pl[i]->cpu_burst_time - pl[i]->io_burst_time + 1;
         sum = sum + wt;
         rl[pl[i]->pid] = wt;
     }
