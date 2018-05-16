6ecc326a670c8b72e7503d791c1ad5817fc8c348
HenryLee <gbvrcx@gmail.com>
2018-05-16 12:31:13 +0900

    update print for time above 10

--- src/record.c
+++ src/record.c
@@ -54,18 +54,18 @@ void write (record_t * this, int pid) {
 void show_gantt_chart (record_t * this) {
     NODE * temp = this->head;
     int time = 0;
-    printf ("0----+----+----+----+----5----+----+----+----+----+\n");
+    printf ("%3d0----+----+----+----+----5----+----+----+----+----+\n   ", time / 10);
     while (temp != NULL) {
         printf ("| %2d ", temp->pid);
         temp = temp->next;
         time++;
         if (!(time % 10)) {
             printf ("|\n");
-            printf ("0----+----+----+----+----5----+----+----+----+----+\n");
+            printf ("%3d0----+----+----+----+----5----+----+----+----+----+\n   ", time / 10);
         }
     }
     if (time % 10 != 0) {
-        printf ("|\n");
+        printf ("|\n   ");
         for (int i = 0; i < time % 10; i++) {
             printf ("+----");
         } 
