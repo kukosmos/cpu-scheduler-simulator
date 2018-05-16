6fbafec59620f302e98eb5ceb3d9b6a6304e620e
HenryLee <gbvrcx@gmail.com>
2018-05-16 20:20:44 +0900

    function structure

--- src/evaluate.h
+++ src/evaluate.h
@@ -2,6 +2,23 @@
 #define __EVALUATE_H
 
 #include "process.h"
-#include "record.h"
+
+/* evaluate turn around time
+ * by calculate termination time - arrival time
+ * return average turn around time
+ * process' turn around time will be stored in return_list
+ */
+double evaluate_turn_around_time (int n, process_t * process_list, double * return_list);
+
+/* evaluate waiting time
+ * by calculate termination time - arrival time - cpu & io burst time
+ * return average waiting time
+ * process' waiting time whill be stored in return_list
+ */
+double evaluate_waiting_time (int n, process_t * process_list, double * return_list);
+
+/* print with form
+ */
+void print_list (char * desc, int n, double * list);
 
 #endif
