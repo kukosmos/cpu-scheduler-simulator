f0aa063efa0da73c6e61bfa7dbae9f77eb889ea2
HenryLee <gbvrcx@gmail.com>
2018-05-16 12:06:49 +0900

    default option

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 #include "simulate.h"
+#include "option.h"
 #include "clock.h"
 #include "job_scheduler.h"
 #include "cpu_scheduler.h"
@@ -10,100 +11,119 @@
 #include "io_device.h"
 
 int main (int argc, char ** argv) {
-    printf("KOSMOS PROJECT\nCPU Scheduler Simulator\n");
+    printf ("KOSMOS PROJECT\nCPU Scheduler Simulator\n");
 
     // default setting
     srand (time (NULL));
 
-    int num_of_processes = 10;
-    int rr_time_quantum = 4;
+    option_t opt;
+    parse_option (argc, argv, &opt);
 
     clk_t * clk = create_clock ();
     job_scheduler_t * js = create_job_scheduler (clk);
     cpu_t * cpu = create_cpu (clk);
     io_device_t * io = create_io_device (clk);
+    cpu_scheduler_t * cs;
     
-    create_processes (js, num_of_processes);
+    create_processes (js, opt.num_of_processes);
     print_processes (js);
 
     // FCFS
-    cpu_scheduler_t * cs = create_cpu_scheduler ("fcfs", clk);
-    start_simulate (clk, js, cs, cpu, io);
+    if (opt.test_fcfs) {
+        cs = create_cpu_scheduler ("fcfs", clk);
+        start_simulate (clk, js, cs, cpu, io);
 
-    printf("FCFS gantt chart\n");
-    show_gantt_chart (cpu->record);
-    printf("IO device gantt chart\n");
-    show_gantt_chart (io->record);
+        printf ("FCFS gantt chart\n");
+        show_gantt_chart (cpu->record);
+        // printf ("IO device gantt chart\n");
+        // show_gantt_chart (io->record);
+
+        delete_cpu_scheduler (cs);
+    }
 
     // non-preemptive SJF
-    delete_cpu_scheduler (cs);
-    cs = create_cpu_scheduler ("np_sjf", clk);
-    reset_record (cpu->record);
-    reset_record (io->record);
-    reset_job_scheduling (js);
-    start_simulate (clk, js, cs, cpu, io);
+    if (opt.test_np_sjf) {
+        cs = create_cpu_scheduler ("np_sjf", clk);
+        reset_record (cpu->record);
+        reset_record (io->record);
+        reset_job_scheduling (js);
+        start_simulate (clk, js, cs, cpu, io);
 
-    printf("Non-preemptive SJF gantt chart\n");
-    show_gantt_chart (cpu->record);
-    printf("IO device gantt chart\n");
-    show_gantt_chart (io->record);
+        printf ("Non-preemptive SJF gantt chart\n");
+        show_gantt_chart (cpu->record);
+        // printf ("IO device gantt chart\n");
+        // show_gantt_chart (io->record);
 
+        delete_cpu_scheduler (cs);
+    }
+    
     // preemptive SJF
-    delete_cpu_scheduler (cs);
-    cs = create_cpu_scheduler ("p_sjf", clk);
-    reset_record (cpu->record);
-    reset_record (io->record);
-    reset_job_scheduling (js);
-    start_simulate (clk, js, cs, cpu, io);
+    if (opt.test_p_sjf) {
+        cs = create_cpu_scheduler ("p_sjf", clk);
+        reset_record (cpu->record);
+        reset_record (io->record);
+        reset_job_scheduling (js);
+        start_simulate (clk, js, cs, cpu, io);
 
-    printf("Preemptive SJF gantt chart\n");
-    show_gantt_chart (cpu->record);
-    printf("IO device gantt chart\n");
-    show_gantt_chart (io->record);
+        printf ("Preemptive SJF gantt chart\n");
+        show_gantt_chart (cpu->record);
+        // printf ("IO device gantt chart\n");
+        // show_gantt_chart (io->record);
+
+        delete_cpu_scheduler (cs);
+    }
 
     // non-preemptive priority
-    delete_cpu_scheduler (cs);
-    cs = create_cpu_scheduler ("np_priority", clk);
-    reset_record (cpu->record);
-    reset_record (io->record);
-    reset_job_scheduling (js);
-    start_simulate (clk, js, cs, cpu, io);
+    if (opt.test_np_priority) {
+        cs = create_cpu_scheduler ("np_priority", clk);
+        reset_record (cpu->record);
+        reset_record (io->record);
+        reset_job_scheduling (js);
+        start_simulate (clk, js, cs, cpu, io);
 
-    printf ("Non-preemptive priority gantt chart\n");
-    show_gantt_chart (cpu->record);
-    printf("IO device gantt chart\n");
-    show_gantt_chart (io->record);
+        printf ("Non-preemptive priority gantt chart\n");
+        show_gantt_chart (cpu->record);
+        // printf ("IO device gantt chart\n");
+        // show_gantt_chart (io->record);
 
+        delete_cpu_scheduler (cs);
+    }
+    
     // preemptive priority
-    delete_cpu_scheduler (cs);
-    cs = create_cpu_scheduler ("p_priority", clk);
-    reset_record (cpu->record);
-    reset_record (io->record);
-    reset_job_scheduling (js);
-    start_simulate (clk, js, cs, cpu, io);
+    if (opt.test_p_priority) {
+        cs = create_cpu_scheduler ("p_priority", clk);
+        reset_record (cpu->record);
+        reset_record (io->record);
+        reset_job_scheduling (js);
+        start_simulate (clk, js, cs, cpu, io);
 
-    printf ("Preemptive priority gantt chart\n");
-    show_gantt_chart (cpu->record);
-    printf("IO device gantt chart\n");
-    show_gantt_chart (io->record);
+        printf ("Preemptive priority gantt chart\n");
+        show_gantt_chart (cpu->record);
+        // printf ("IO device gantt chart\n");
+        // show_gantt_chart (io->record);
 
+        delete_cpu_scheduler (cs);
+    }
+    
     // round robin
-    delete_cpu_scheduler (cs);
-    cs = create_cpu_scheduler ("rr", clk, rr_time_quantum);
-    reset_record (cpu->record);
-    reset_record (io->record);
-    reset_job_scheduling (js);
-    start_simulate (clk, js, cs, cpu, io);
+    if (opt.test_rr) {
+        cs = create_cpu_scheduler ("rr", clk, opt.round_robin_time_quantum);
+        reset_record (cpu->record);
+        reset_record (io->record);
+        reset_job_scheduling (js);
+        start_simulate (clk, js, cs, cpu, io);
 
-    printf ("Round-robin gantt chart\n");
-    show_gantt_chart (cpu->record);
-    printf("IO device gantt chart\n");
-    show_gantt_chart (io->record);
+        printf ("Round-robin gantt chart\n");
+        show_gantt_chart (cpu->record);
+        // printf ("IO device gantt chart\n");
+        // show_gantt_chart (io->record);
 
+        delete_cpu_scheduler (cs);
+    }
+    
     // free all allocated memory
     delete_io_device (io);
     delete_cpu (cpu);
-    delete_cpu_scheduler (cs);
     delete_job_scheduler (js);
     delete_clock (clk);
 
---
+++ src/option.c
@@ -0,0 +1,18 @@
+#include "option.h"
+
+#define TRUE 1
+#define FALSE 0
+
+#define DEFAULT_NUM_PROCESS 10
+#define DEFAULT_TIME_QUANTUM 4
+
+void parse_option (int argc, char ** argv, option_t * opt) {
+    opt->test_fcfs = TRUE;
+    opt->test_np_sjf = TRUE;
+    opt->test_p_sjf = TRUE;
+    opt->test_np_priority = TRUE;
+    opt->test_p_priority = TRUE;
+    opt->test_rr = TRUE;
+    opt->num_of_processes = DEFAULT_NUM_PROCESS;
+    opt->round_robin_time_quantum = DEFAULT_TIME_QUANTUM;
+}
