f389d30e6d4cfc4d40be393fdb2b2b203f47c950
HenryLee <gbvrcx@gmail.com>
2018-05-16 11:01:23 +0900

    implement round robin

--- src/algorithm/rr.h
+++ src/algorithm/rr.h
@@ -1,4 +1,19 @@
 #ifndef __RR_H
 #define __RR_H
 
+#include "../process.h"
+#include "../cpu_scheduler.h"
+
+/* round robin queue is FIFO queue
+ */
+void * create_rr_queue (int time_quantum);
+
+/* function for cpu scheduling
+ */
+void rr_scheduling (cpu_scheduler_t * cpu_scheduler);
+
+/* round robin enqueue
+ */
+void rr_enqueue (void * queue, process_t * process);
+
 #endif
