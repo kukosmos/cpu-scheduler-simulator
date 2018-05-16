e51d5dff8fb561e9cf2151b840d70f78a151a1d7
HenryLee <gbvrcx@gmail.com>
2018-05-16 12:54:00 +0900

    default option

--- src/option.c
+++ src/option.c
@@ -7,12 +7,20 @@
 #define DEFAULT_TIME_QUANTUM 4
 
 void parse_option (int argc, char ** argv, option_t * opt) {
-    opt->test_fcfs = TRUE;
-    opt->test_np_sjf = TRUE;
-    opt->test_p_sjf = TRUE;
-    opt->test_np_priority = TRUE;
-    opt->test_p_priority = TRUE;
-    opt->test_rr = TRUE;
-    opt->num_of_processes = DEFAULT_NUM_PROCESS;
-    opt->round_robin_time_quantum = DEFAULT_TIME_QUANTUM;
+    if (argc == 1) {
+        // no argument default
+        opt->test_fcfs = TRUE;
+        opt->test_np_sjf = TRUE;
+        opt->test_p_sjf = TRUE;
+        opt->test_np_priority = TRUE;
+        opt->test_p_priority = TRUE;
+        opt->test_rr = TRUE;
+        opt->num_of_processes = DEFAULT_NUM_PROCESS;
+        opt->round_robin_time_quantum = DEFAULT_TIME_QUANTUM;
+        return;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        
+    }
 }
